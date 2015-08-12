8da288b43000237ffc5419f209ea051717e943e8
webreflection <andrea.giammarchi@gmail.com>
2015-08-12 20:29:38 +0100

    added README and description and full tests coverage

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
-notify-js
+notify-js [![build status](https://secure.travis-ci.org/WebReflection/notify-js.svg)](http://travis-ci.org/WebReflection/notify-js)
 =========
+A simplified notification channel for global, or local, interaction.
 
-[![build status](https://secure.travis-ci.org/WebReflection/notify-js.png)](http://travis-ci.org/WebReflection/notify-js)
 
+### API
+There are 4 methods, described as such:
+
+  * `notify.when(type, callback)` to add a listener associated to a specific type/event. If such type was already resolved, it will synchronously invoke the callback.
+  * `notify.about(type, value)` to resolve a type/event with a value. Whatever listener was waiting for it, it will be invoked with such value passed as argument. Whatever listener will be added in the future through `when` it'll be instantly resolved with such value.
+  * `notify.drop(type, callback)` in case something hasn't happened yet and we changed our mind about waiting for the event, we can still remove it!
+  * `notify.new()` create a new `notify`-like object. By default, `notify` is a global communication channel, but it brings this simple method that will create a new object for private communication purpose, if needed.
+
+In order to use private channels, feel free to create unique IDs as type, or simply use a `Symbol`, whenever available.
+
+
+### Compatibility
+Notify has been created in a full cross platform, browser, engine way, and it's compatible down to IE6 on Desktop, and every mobile browser I could test.
+
+If it wasn't for [this Espruino bug](https://github.com/espruino/Espruino/issues/561) it would have worked in there too.
--- test/notify-js.js
+++ test/notify-js.js
@@ -1,13 +1,72 @@
 //remove:
-var main = require('../build/notify-js.node.js');
+var notify = require('../build/notify-js.node.js');
 //:remove
 
 wru.test([
   {
     name: "main",
     test: function () {
-      wru.assert(typeof main == "object");
+      wru.assert(typeof notify == "object");
       // wru.assert(0);
+    }
+  },{
+    name: 'when',
+    test: function  () {
+      var i = 0, r = Math.random();
+      function increase() { i++; }
+      notify.when('test-when', increase);
+      wru.assert('did not happen', i === 0);
+      notify.about('test-when', r);
+      wru.assert('it was invoked', i === 1);
+      notify.about('test-when', r);
+      wru.assert('it was NOT invoked', i === 1);
+      notify.when('test-when', increase);
+      wru.assert('it was invoked again', i === 2);
+    }
+  },{
+    name: 'about',
+    test: function () {
+      var
+        r1 = Math.random(),
+        r2 = r1 + Math.random(),
+        v
+      ;
+      function about(value) { v = value; }
+      notify.when('test-about', about);
+      wru.assert(v === undefined);
+      notify.about('test-about', r1);
+      wru.assert(v === r1);
+      v = null;
+      notify.when('test-about', about);
+      notify.about('test-about', r2);
+      notify.when('test-about', about);
+      wru.assert(v === r2);
+    }
+  },{
+    name: 'drop',
+    test: function () {
+      var i = 0, r = Math.random();
+      function increase() { i++; }
+      notify.when('test-drop', increase);
+      notify.drop('test-drop', increase);
+      notify.about('test-drop', r);
+      wru.assert('it was NOT invoked', i === 0);
+    }
+  },{
+    name: 'new',
+    test: function () {
+      var other = notify['new']();
+      var which = '', r = Math.random();
+      other.when('test-new', function () {
+        which += 'other';
+      });
+      notify.when('test-new', function () {
+        which += 'notify';
+      });
+      other.about('test-new', r);
+      wru.assert('it was invoked as other', which === 'other');
+      notify.about('test-new', r);
+      wru.assert('it was invoked as notify', which === 'othernotify');
     }
   }
 ]);
